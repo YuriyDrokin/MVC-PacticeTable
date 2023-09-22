6121250cd9170deddbdf126e94b5f8ff4e17216b
YuriySask <yd.appdeveloper@gmail.com>
2023-09-22 11:20:03 -0600

    Retrieve All Categories
    Retrieve database set and store in objCategoryList variable, we don't need write sql Statement tp retrieve data from database.

--- MVC-SellingObjectsApp/MVC-SellingObjectsApp/Controllers/CategoryController.cs
+++ MVC-SellingObjectsApp/MVC-SellingObjectsApp/Controllers/CategoryController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_SellingObjectsApp.Data;
 
 namespace MVC_SellingObjectsApp.Controllers
 {
     public class CategoryController : Controller
     {
+        //access database object
+        private readonly ApplicationDbContext _db;
+
+        // using constructoir whatever register inside the container we can get implementation of the connection and retrieve the Data
+         public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            var objCategoryList = _db.Categories.ToList(); // accessing to db set and converting to a list and store in ObjCategoryList, no need sql statements 
             return View();
         }
     }
