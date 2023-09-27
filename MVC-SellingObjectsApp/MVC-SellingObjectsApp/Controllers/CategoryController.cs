21475e21b41c42c53a2e2e55c3f3e70ad8ad83a7
YuriySask <yd.appdeveloper@gmail.com>
2023-09-26 19:24:39 -0600

    Creating New Category
    Creating new category data using cross site request forgery

--- MVC-SellingObjectsApp/MVC-SellingObjectsApp/Controllers/CategoryController.cs
+++ MVC-SellingObjectsApp/MVC-SellingObjectsApp/Controllers/CategoryController.cs
@@ -25,5 +25,14 @@ namespace MVC_SellingObjectsApp.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index"); // redirecto to index
+        }
     }
 }
