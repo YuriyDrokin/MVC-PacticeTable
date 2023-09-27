593ee0ba9ab27846e5ba779aaed01130b80f0602
YuriySask <yd.appdeveloper@gmail.com>
2023-09-27 10:15:32 -0600

    Edit Category - Get
    Add in CategoryController new options Edit
    Add new page Edit
    Add button edit

--- MVC-SellingObjectsApp/MVC-SellingObjectsApp/Controllers/CategoryController.cs
+++ MVC-SellingObjectsApp/MVC-SellingObjectsApp/Controllers/CategoryController.cs
@@ -20,12 +20,12 @@ namespace MVC_SellingObjectsApp.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories; // accessing to db set and converting to a list and store in ObjCategoryList, no need sql statements 
             return View(objCategoryList);
         }
-
+        //GET
         public IActionResult Create()
         {
             return View();
         }
-
+        //POST
         [HttpPost]
         [ValidateAntiForgeryToken] // cross site request forgery and AntoForgery token
         public IActionResult Create(Category obj)
@@ -40,6 +40,43 @@ namespace MVC_SellingObjectsApp.Controllers
             _db.Categories.Add(obj); // creating record in database just addin into database
             _db.SaveChanges();// that will push into database when we save all the changes
             return RedirectToAction("Index"); // redirecto to index page to what happened 
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if(categoryFromDb == null) {
+            return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] // cross site request forgery and AntoForgery token
+        public IActionResult Edit(Category obj)
+        {
+            // Validation for entering same name and Validorder value
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot MATCH the Name");
+            }
+
+            if (ModelState.IsValid)
+            { // check validation
+                _db.Categories.Add(obj); // creating record in database just addin into database
+                _db.SaveChanges();// that will push into database when we save all the changes
+                return RedirectToAction("Index"); // redirecto to index page to what happened 
             }
             return View(obj);
         }
--- MVC-SellingObjectsApp/MVC-SellingObjectsApp/Models/Category.cs
+++ MVC-SellingObjectsApp/MVC-SellingObjectsApp/Models/Category.cs
@@ -9,7 +9,9 @@ namespace MVC_SellingObjectsApp.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        //modifying attributes
         [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "Display Order must be between 1 and 100 only")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now; // assigned time to now when created
     }
